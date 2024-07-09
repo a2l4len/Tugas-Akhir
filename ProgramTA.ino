ef86c28f1eedac5ab653db4f8e259722d771463c
a2l4len <130351088+a2l4len@users.noreply.github.com>
2024-07-10 02:53:41 +0700

    ProgramTA.ino

--- ProgramTA.ino
+++ ProgramTA.ino
@@ -122,19 +122,16 @@ void setup() {
   if (myFile) {
     Serial.println("PARAME~1.txt");
 
-    // Read from the file until there's nothing else in it
+    // baca file dari micro-SD Card
     while (myFile.available()) {
       String data = myFile.readStringUntil('\n');
-//      Serial.println(data);
       
-      // Assuming the variable is stored as 'variableName=value'
       int separatorIndex = data.indexOf('=');
       if (separatorIndex != -1) {
         String variableName = data.substring(0, separatorIndex);
         String valueString = data.substring(separatorIndex + 1);
-        float value = valueString.toFloat(); // Convert string to float
+        float value = valueString.toFloat(); 
         
-        // Store the value in the corresponding variable
         if (variableName == "voltage1minimum") {
           voltage1minimum = value;
         } else if (variableName == "voltage1maximum") {
@@ -166,40 +163,10 @@ void setup() {
         }
       }
     }
-    // Close the file
     myFile.close();
     
-    // Print the stored variables
-//    Serial.print("voltage1minimum: ");
-//    Serial.println(voltage1minimum);
-//    Serial.print("voltage1maximum: ");
-//    Serial.println(voltage1maximum);
-//    Serial.print("voltage2minimum: ");
-//    Serial.println(voltage2minimum);
-//    Serial.print("voltage2maximum: ");
-//    Serial.println(voltage2maximum);
-//    Serial.print("voltage3minimum: ");
-//    Serial.println(voltage3minimum);
-//    Serial.print("voltage3maximum: ");
-//    Serial.println(voltage3maximum);
-//    Serial.print("voltage4minimum: ");
-//    Serial.println(voltage4minimum);
-//    Serial.print("voltage4maximum: ");
-//    Serial.println(voltage4maximum);
-//    Serial.print("voltage5minimum: ");
-//    Serial.println(voltage5minimum);
-//    Serial.print("voltage5maximum: ");
-//    Serial.println(voltage5maximum);
-//    Serial.print("voltage6minimum: ");
-//    Serial.println(voltage6minimum);
-//    Serial.print("voltage6maximum: ");
-//    Serial.println(voltage6maximum);
-//    Serial.print("voltage7minimum: ");
-//    Serial.println(voltage7minimum);
-//    Serial.print("voltage7maximum: ");
-//    Serial.println(voltage7maximum);
+
   } else {
-    // If the file didn't open, print an error
     Serial.println("Error opening PARAME~1.txt");
   }
 }
@@ -223,10 +190,6 @@ void loop() {
       currentStats.input(currentRawValue); 
       sinusWaveStats.input(signal1); 
   
-//      Serial.print(currentRawValue);
-//      Serial.print(" "); 
-//      Serial.print(signal1); 
-//      Serial.print(" "); 
   
     if ((unsigned long)(millis() - previousMillis) >= printPeriod) { 
       previousMillis = millis(); 
@@ -234,10 +197,6 @@ void loop() {
       Amps_TRMS = intercept + slope * currentStats.sigma(); 
   
     }
-//      Serial.print(currentStats.sigma());
-//      Serial.print(" ");
-//      Serial.print(Amps_TRMS);
-//      Serial.print(" ");
     }
   
   if (distance == 1){
@@ -248,7 +207,6 @@ void loop() {
       screen.clear();
       screen.setCursor(0,0);
       screen.print("PROCESSING...");
-//      Serial.println ("Processing");
       delay (2000);
       displayed = true;
       screen.clear();
@@ -263,8 +221,6 @@ void loop() {
               if (voltage5 > voltage5minimum && voltage5 < voltage5maximum){
                 if (voltage6 > voltage6minimum && voltage6 < voltage6maximum){
                   if (voltage7 > voltage7minimum && voltage7 < voltage7maximum){
-//                    screen.clear();
-//                    delay (100);
                     screen.setCursor(0,0);
                     screen.print("GOOD");
                   }
@@ -284,7 +240,7 @@ void loop() {
       }
 
       if (voltage3 < voltage3minimum || voltage3 > voltage3maximum) {
-        errormessages += "TITIK 5 V ";
+        errormessages += "TITIK 9 V ";
       }
 
       if (voltage4 < voltage4minimum || voltage4 > voltage4maximum) {
@@ -315,9 +271,6 @@ void loop() {
     screen.setCursor(0,0);
     screen.print("STAND BY");
     displayed = false;
-//    signal1 = 0;
-//    signal2 = 0;
-//    Amps_TRMS = 0;
     voltage1 = 0;
     voltage2 = 0;
     voltage3 = 0;
@@ -325,9 +278,6 @@ void loop() {
     voltage5 = 0;
     voltage6 = 0;
     voltage7 = 0;
-//    delay (200);
-//    screen.clear();
-//    delay (2000);
 //    voltage8 = 0;
 //    voltage9 = 0;
 //    voltage10 = 0;
@@ -382,15 +332,11 @@ void signal_reading(){
   float adc_value = data * 4.75 / 1024;
 
   signal1 = mapFloat(adc_value, 0.0, 4.75, -20, 20);
-//  Serial.print (signal1);
-//  Serial.print (" ");
 
   float datasignal = analogRead(A1);
   float adc_signal = datasignal * 4.75 / 1024;
 
   signal2 = mapFloat(adc_signal, 0.0, 4.75, -20, 20);
-//  Serial.print (signal2);
-//  Serial.print (" ");
 }
 
 
@@ -401,117 +347,81 @@ void MeasureVoltage(){
   float adc_value1 = data1 * 4.75 / 1024;
 
   voltage1 = mapFloat(adc_value1, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage1);
-//  Serial.print (" ");
-  //Titik 1
+  //Titik 15 Volt
   
   float data2 = analogRead(A3);
   float adc_value2 = data2 * 4.75 / 1024;
 
   voltage2 = mapFloat(adc_value2, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage2);
-//  Serial.print (" ");
-  //Titik 2
+  //Titik -15 Volt
   
   float data3 = analogRead(A4);
   float adc_value3 = data3 * 4.75 / 1024;
 
   voltage3 = mapFloat(adc_value3, 0.0, 4.75, -20, 25);
-  //Serial.println ("V5 = ");
-//  Serial.print (voltage3);
-//  Serial.print (" ");
-  //Titik 3
+  //Titik 9 Volt
 
   float data4 = analogRead(A5);
   float adc_value4 = data4 * 4.75 / 1024;
 
   voltage4 = mapFloat(adc_value4, 0.0, 4.75, -40, 45.5);
-//  Serial.print (voltage4);
-//  Serial.print (" ");
-
-  //Titik 4  
+  //Titik 30 Volt 
   
   float data5 = analogRead(A6);
   float adc_value5 = data5 * 4.75 / 1024;
   voltage5 = mapFloat(adc_value5, 0.0, 4.75, -40, 45.5);
-//  Serial.print (voltage5);
-//  Serial.print (" ");
-
-  //Titik 5
+  //Titik -30 Volt
 
   float data6 = analogRead(A7);
   float adc_value6 = data6 * 4.75 / 1024;
 
   voltage6 = mapFloat(adc_value6, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage6);
-//  Serial.print (" ");
-
-  //Titik 6
+  //Titik 0.6 Volt
 
   float data7 = analogRead(A8);
   float adc_value7 = data7 * 4.75 / 1024;
 
   voltage7 = mapFloat(adc_value7, 0.0, 4.75, -20, 25);
-//  Serial.println (voltage7);
-//  Serial.print (" ");
-//  delayMicroseconds(500);
-  //Titik 7
+  //Titik 1.2 Volt
 
 //  float data8 = analogRead(A9);
 //  float adc_value8 = data8 * 4.75 / 1024;
-//
+
 //  voltage8 = mapFloat(adc_value8, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage8);
-//  Serial.print (" ");
-////  delayMicroseconds(500);
-//  //Titik 8
-//
+  //Titik 8
+
 //  float data9 = analogRead(A10);
 //  float adc_value9 = data9 * 4.75 / 1024;
 //
 //  voltage9 = mapFloat(adc_value9, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage9);
-//  Serial.print (" ");
-////  delayMicroseconds(500);
-//  //Titik 9
-//
+  //Titik 9
+
 //  float data10 = analogRead(A11);
 //  float adc_value10 = data10 * 4.75 / 1024;
-//
+
 //  voltage10 = mapFloat(adc_value10, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage10);
-//  Serial.print (" ");
-////  delayMicroseconds(500);
-//  //Titik 10
+  //Titik 10
 //
 //  float data11 = analogRead(A12);
 //  float adc_value11 = data11 * 4.75 / 1024;
-//
+
 //  voltage11 = mapFloat(adc_value11, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage11);
-//  Serial.print (" ");
-////  delayMicroseconds(500);
-//  //Titik 11
+  //Titik 11
 //  
 //  float data12 = analogRead(A13);
 //  float adc_value12 = data12 * 4.75 / 1024;
-//
+
 //  voltage12 = mapFloat(adc_value12, 0.0, 4.75, -20, 25);
-//  Serial.print (voltage12);
-//  Serial.print (" ");
-////  delayMicroseconds(500);
-//  //Titik 12
-//
+  //Titik 12
+
 //  float data13 = analogRead(A14);
 //  float adc_value13 = data13 * 4.75 / 1024;
-//
+
 //  voltage13 = mapFloat(adc_value13, 0.0, 4.75, -20, 25);
-//  Serial.println (voltage13);
-//  //Titik 13
-//  delayMicroseconds(250); 
+  //Titik 13
 }
 
-
+//untuk scroll tulisan titik yang error
 void scrollMessage(int row, String message, int delayTime, int totalColumns) {
   for (int i=0; i < totalColumns; i++) {
     message = " " + message;  
